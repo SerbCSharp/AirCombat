91b13b080da6268a51de551b44184e05dd92abbf
SerbCSharp <serbcsharp@mail.ru>
2025-02-11 14:50:43 +0300

    add plane

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.inner
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 40;
-//camera.lookAt(scene.position);
+camera.lookAt(scene.position);
 
 const axes = new THREE.AxesHelper(5);
 scene.add(axes);
@@ -21,35 +21,20 @@ scene.add(spotLight );
 
 const planeGeometry = new THREE.PlaneGeometry(28.2,48.6);
 const planeMaterial = new THREE.MeshBasicMaterial( { map: texture } );
-const planeMaterial1 = new THREE.MeshBasicMaterial( { color: 0xfcc742, wireframe: true } );
 const plane = new THREE.Mesh( planeGeometry, planeMaterial );
 plane.position.x = 0
-plane.position.y = 1;
+plane.position.y = 0;
 plane.position.z = 0
 plane.rotation.z = -0.5*Math.PI;
-
-
-
-const planeGeometry1 = new THREE.PlaneGeometry(26,46,13,23);
-const plane1 = new THREE.Mesh( planeGeometry1, planeMaterial1 );
-plane1.position.x = 0;
-plane1.position.y = 0;
-plane1.position.z = 0;
-plane1.rotation.z = -0.5*Math.PI;
-
-scene.add( plane1 );
-
 scene.add( plane );
 
-const cubes = [];
-const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xfcc742 });
+const cubeGeometry = new THREE.BoxGeometry(2,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'blue' });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.position.y = 0;
+cube.position.y = 1;
 cube.position.x = 1;
-cube.position.z = 1;
+cube.position.z = 0.6;
 scene.add(cube);
-cubes.push(cube);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 renderer.setSize( window.innerWidth, window.innerHeight );
